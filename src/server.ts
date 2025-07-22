0fe366db838e3d91a1508f22959f12a08ce97465
TomasGarciaDev <tomasgarciadev@gmail.com>
2025-07-22 15:35:36 +0100

    Feat: server resources template

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
-import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
 import fs from "node:fs/promises";
@@ -32,6 +35,44 @@ server.resource(
         {
           uri: uri.href,
           text: JSON.stringify(users),
+          mimeType: "application/json",
+        },
+      ],
+    };
+  }
+);
+
+server.resource(
+  "user-details",
+  new ResourceTemplate("users://{userId}/profile", { list: undefined }),
+  {
+    description: "Get a user's details from the database",
+    title: "User Details",
+    mimeType: "application/json",
+  },
+  async (uri, { userId }) => {
+    const users = await import("./data/users.json", {
+      with: { type: "json" },
+    }).then((m) => m.default);
+    const user = users.find((u) => u.id === parseInt(userId as string));
+
+    if (!user) {
+      return {
+        contents: [
+          {
+            uri: uri.href,
+            text: JSON.stringify({ error: "User not found" }),
+            mimeType: "application/json",
+          },
+        ],
+      };
+    }
+
+    return {
+      contents: [
+        {
+          uri: uri.href,
+          text: JSON.stringify(user),
           mimeType: "application/json",
         },
       ],
